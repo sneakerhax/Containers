4258f48239f3e548e47dd459fe35e525710aa861
sneakerhax <sneakerhax@gmail.com>
2025-01-28 16:24:10 -0800

    Updated dockerhub_updates.sh to use buildx

--- dockerhub_update.sh
+++ dockerhub_update.sh
@@ -1,15 +1,15 @@
 # build and push nmap-small to Docker Hub
-docker build -t sneakerhax/nmap-small -f Nmap-small/Dockerfile Nmap-small/
-docker push sneakerhax/nmap-small:latest
+docker buildx build --platform linux/amd64 -t sneakerhax/nmap-small -f Nmap-small/Dockerfile Nmap-small/ --push
+# docker push sneakerhax/nmap-small:latest
 
 # build and push nmap to Docker Hub
-docker build -t sneakerhax/nmap -f Nmap/Dockerfile Nmap/
-docker push sneakerhax/nmap:latest
+docker buildx build --platform linux/amd64 -t sneakerhax/nmap -f Nmap/Dockerfile Nmap/ --push
+# docker push sneakerhax/nmap:latest
 
 # build and push metasploit to Docker Hub
-docker build -t sneakerhax/metasploit -f Metasploit/Dockerfile Metasploit/
-docker push sneakerhax/metasploit:latest
+docker buildx build --platform linux/amd64 -t sneakerhax/metasploit -f Metasploit/Dockerfile Metasploit/ --push
+# docker push sneakerhax/metasploit:latest
 
 # build and push wordlists to Docker Hub
-docker build -t sneakerhax/wordlists -f Wordlists/Dockerfile Wordlists/
-docker push sneakerhax/wordlists:latest
+docker buildx build --platform linux/amd64 -t sneakerhax/wordlists -f Wordlists/Dockerfile Wordlists/ --push
+# docker push sneakerhax/wordlists:latest
