d355b62d15e81a1fd5d59f491c73c79813b3062b
sneakerhax <sneakerhax@gmail.com>
2025-10-06 22:26:21 -0700

    Update dockerhub_update to push both linux/amd64 and linux/arm64 images

--- dockerhub_update.sh
+++ dockerhub_update.sh
@@ -1,15 +1,15 @@
 # build and push nmap-small to Docker Hub
-docker buildx build --platform linux/amd64 -t sneakerhax/nmap-small -f Nmap-small/Dockerfile Nmap-small/ --push
+docker buildx build --platform linux/amd64,linux/arm64 -t sneakerhax/nmap-small -f Nmap-small/Dockerfile Nmap-small/ --push
 # docker push sneakerhax/nmap-small:latest
 
 # build and push nmap to Docker Hub
-docker buildx build --platform linux/amd64 -t sneakerhax/nmap -f Nmap/Dockerfile Nmap/ --push
+docker buildx build --platform linux/amd64,linux/arm64 -t sneakerhax/nmap -f Nmap/Dockerfile Nmap/ --push
 # docker push sneakerhax/nmap:latest
 
 # build and push metasploit to Docker Hub
-docker buildx build --platform linux/amd64 -t sneakerhax/metasploit -f Metasploit/Dockerfile Metasploit/ --push
+docker buildx build --platform linux/amd64,linux/arm64 -t sneakerhax/metasploit -f Metasploit/Dockerfile Metasploit/ --push
 # docker push sneakerhax/metasploit:latest
 
 # build and push wordlists to Docker Hub
-docker buildx build --platform linux/amd64 -t sneakerhax/wordlists -f Wordlists/Dockerfile Wordlists/ --push
+docker buildx build --platform linux/amd64,linux/arm64 -t sneakerhax/wordlists -f Wordlists/Dockerfile Wordlists/ --push
 # docker push sneakerhax/wordlists:latest
